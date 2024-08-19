88702d26ea7fd69a5097ca14e8f46653af349048
K85246 <karim.s.s@hotmail.com>
2024-08-19 12:49:20 +0300

    add has function to hash the password of users

--- src/App.java
+++ src/App.java
@@ -1,14 +1,26 @@
 import java.sql.Statement;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
 import java.util.Base64;
 import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileReader;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -19,28 +31,29 @@ public class App {
         String[] data = FileReader("Data/data.txt");
         Database database = new Database();
         database.selectQuery();
-        
-        
-
+        database.close();
         // KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        // keyGen.init(256);
-        // SecretKey key = keyGen.generateKey();
-        // byte[] iv = new byte[16];
+        // keyGen.init(128);
+        // SecretKey secretKey = keyGen.generateKey();
+        // byte[] key = secretKey.getEncoded();
+        // System.out.println("the key: "+Base64.getEncoder().encodeToString(key));
+        // byte[] IV = new byte[16];
         // SecureRandom random = new SecureRandom();
-        // random.nextBytes(iv);
-        // IvParameterSpec IvParam = new IvParameterSpec(iv);
+        // random.nextBytes(IV);
+        // IvParameterSpec IvParameterSpec = new IvParameterSpec(IV);
+        // Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         // for (String plainText : data) {
-        //     Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5padding");
-        //     cipher.init(cipher.ENCRYPT_MODE, key,IvParam);
+        //     cipher.init(Cipher.ENCRYPT_MODE,secretKey,IvParameterSpec);
         //     byte[] cipherText = cipher.doFinal(plainText.getBytes());
-        //     System.out.println("The encrypted Text: "+cipherText);
-        //     cipher.init(cipher.DECRYPT_MODE, key,IvParam);
-        //     byte[] originText = cipher.doFinal(cipherText);
-        //     System.out.println("The origin Text : "+ originText);
+        //     System.out.println("encrypted string: "+ Base64.getEncoder().encodeToString(cipherText));
+
+        //     cipher.init(Cipher.DECRYPT_MODE, secretKey,IvParameterSpec);
+        //     byte[] decryptedText = cipher.doFinal(cipherText);
+        //     System.out.println("Decrypted string: " + new String(decryptedText));
         // }
     }
 
-    String[] FileReader(String FileName) throws FileNotFoundException{
+    static String[] FileReader(String FileName) throws FileNotFoundException{
         File file = new File(FileName);
         int LineCount = 0;
         String[] line;
@@ -59,5 +72,17 @@ public class App {
             }
         }
         return line;
+    }
+
+    static String HashSHA256(String data){
+        String EncryptedPassword = null;
+        try{
+        MessageDigest message = MessageDigest.getInstance("SHA-256");
+        byte[] bytes =message.digest(data.getBytes(StandardCharsets.UTF_8));
+        EncryptedPassword = Base64.getEncoder().encodeToString(bytes);
+        } catch(NoSuchAlgorithmException e){
+            System.out.println("Algorithm Error: "+e.getMessage());
+        }
+        return EncryptedPassword;
     }
 }
