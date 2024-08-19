94cc4319dabd8bb7eb13beac111407765a838a73
K85246 <karim.s.s@hotmail.com>
2024-08-19 15:10:16 +0300

    change excelsheet class to be with consturctor with gloal private attributes to enhance the class

--- src/excelSheet.java
+++ src/excelSheet.java
@@ -10,22 +10,22 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class excelSheet {
-    static FileInputStream inputFile = null;
-    String excelPath = null;
-    void insert(ResultSet Query){
-         try{
-            excelPath = "C:/Users/karim/Desktop/test.xlsx";
+    private FileInputStream inputFile;
+    private String excelPath;
+    private Workbook workbook;
+    private Sheet sheet;
+
+    public excelSheet(){
+        excelPath = "C:/Users/karim/Desktop/test.xlsx";
+        try{
+            System.out.println("Opening excelSheet...");
             inputFile = new FileInputStream(excelPath);
-            Workbook workbook = new XSSFWorkbook(inputFile);
-            Sheet sheet = workbook.getSheetAt(0);
-            Row row1 = sheet.getRow(0);
-            Cell cell = row1.createCell(2);
-            cell.setCellValue("test");
-            try(FileOutputStream outputStream = new FileOutputStream(excelPath)){
-                workbook.write(outputStream);
-            }
-        } catch(IOException e){
+            workbook = new XSSFWorkbook(inputFile);
+            sheet = workbook.getSheetAt(0);
+            System.out.println("excelsheet opened successfully!");
+        }catch(IOException e){
             System.out.println("error opening excelsheet: "+e.getMessage());
+            
             try{
                 System.out.println("Creating ExcelSheet...");
                 FileOutputStream outputStream = new FileOutputStream(excelPath);
@@ -37,55 +37,56 @@ public class excelSheet {
         }
     }
 
-    void read(){
-        excelPath = "C:/Users/karim/Desktop/test.xlsx";
-        try{
-            inputFile = new FileInputStream(excelPath);
-            Workbook workbook = new XSSFWorkbook(inputFile);
-            Sheet sheet = workbook.getSheetAt(0);
-            System.out.println();
-            for (Row row : sheet) {
-                for (Cell Cell : row) {
-                    switch (Cell.getCellType()) {
-                        case STRING:
-                            System.out.println(Cell.getStringCellValue());
-                            break;
-                        case NUMERIC:
-                            System.out.println(Cell.getNumericCellValue());
-                            break;
-                        case FORMULA:
-                            try {
-                                switch (Cell.getCachedFormulaResultType()) {
-                                    case STRING:
-                                        System.out.print("Formula String: " + Cell.getStringCellValue() + "\t");
-                                        break;
-                                    case NUMERIC:
-                                        System.out.print("Formula Numeric: " + Cell.getNumericCellValue() + "\t");
-                                        break;
-                                    case BOOLEAN:
-                                        System.out.print("Formula Boolean: " + Cell.getBooleanCellValue() + "\t");
-                                        break;
-                                    default:
-                                        System.out.print("Formula Unknown\t");
-                                        break;
-                                }
-                            } catch (IllegalStateException e) {
-                                System.out.print("Error: " + e.getMessage() + "\t");
-                            }
-                            break;
-                        case null:
-                            break;
-                        default:
-                            System.out.println("unknown value!");
-                            break;
-                    }
-                }
-                System.out.println();
-            } 
-        } catch(FileNotFoundException e){
-            System.out.println("error open file: "+e.getMessage());
-        } catch(IOException e){
-            System.out.println("IO error: "+e.getMessage());
+    void insert(ResultSet Query){
+        Row row1 = sheet.getRow(0);
+        Cell cell = row1.createCell(2);
+        cell.setCellValue("test");
+        try(FileOutputStream outputStream = new FileOutputStream(excelPath)){
+            workbook.write(outputStream);
+            System.out.println("inserting excelSheet successfully!");
+        }catch(IOException e){
+            System.out.println("error opening excelsheet: "+e.getMessage());
         }
+    }
+
+    void read(){
+        for (Row row : sheet) {
+            for (Cell Cell : row) {
+                switch (Cell.getCellType()) {
+                    case STRING:
+                        System.out.println(Cell.getStringCellValue());
+                        break;
+                    case NUMERIC:
+                        System.out.println(Cell.getNumericCellValue());
+                        break;
+                    case FORMULA:
+                        try {
+                            switch (Cell.getCachedFormulaResultType()) {
+                                case STRING:
+                                    System.out.print("Formula String: " + Cell.getStringCellValue() + "\t");
+                                    break;
+                                case NUMERIC:
+                                    System.out.print("Formula Numeric: " + Cell.getNumericCellValue() + "\t");
+                                    break;
+                                case BOOLEAN:
+                                    System.out.print("Formula Boolean: " + Cell.getBooleanCellValue() + "\t");
+                                    break;
+                                default:
+                                    System.out.print("Formula Unknown\t");
+                                    break;
+                            }
+                        } catch (IllegalStateException e) {
+                            System.out.print("Error: " + e.getMessage() + "\t");
+                        }
+                        break;
+                    case null:
+                        break;
+                    default:
+                        System.out.println("unknown value!");
+                        break;
+                }
+            }
+            System.out.println();
+        } 
     }
 }
