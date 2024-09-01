a083e1a34a9c780b1ba2145081ce6815d1dbaecb
K85246 <karim.s.s@hotmail.com>
2024-09-01 12:45:03 +0300

    add action listener to run ( which display what we write in TextArea in the label)

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,16 +1,14 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 
 public class MainFrame extends JFrame {
 
     JButton runButton = new JButton("Run");
     JButton clearButton = new JButton("Clear");
     JTextArea text = new JTextArea();
-    
+    JLabel errorLabel = new JLabel();
     
     MainFrame(){
         JPanel mainPanel = new JPanel();
@@ -19,17 +17,29 @@ public class MainFrame extends JFrame {
         setSize(500,600);
         setMinimumSize(new Dimension(400,500));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        errorLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        
 
-
+        JPanel northPanel = new JPanel();
+        //northPanel.setPreferredSize(new Dimension(50,50));
         JPanel centerPanel = new JPanel();
-        centerPanel.setPreferredSize(new Dimension(50, 50));
-        centerPanel.setLayout(new GridLayout(1,1,20,50));
+        //centerPanel.setPreferredSize(new Dimension(50,10));
         JPanel southPanel = new JPanel();
         southPanel.setLayout(new GridLayout(1,2,20,0));
         southPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         
         JPanel runPanel = new JPanel();
         runPanel.add(runButton);
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                if(text.getText().isEmpty()){
+                    errorLabel.setText("NULL");
+                }else{
+                    errorLabel.setText(text.getText());
+                }
+            }
+        });
         JPanel clearPanel = new JPanel();
         clearPanel.add(clearButton);
         clearButton.addActionListener(new ActionListener() {
@@ -40,16 +50,19 @@ public class MainFrame extends JFrame {
         });
         runButton.setPreferredSize(new Dimension(100, 50)); 
         clearButton.setPreferredSize(new Dimension(100, 50));
-
         JPanel textPanel = new JPanel();
-        textPanel.setBorder(BorderFactory.createEmptyBorder(300,0,0,0));
+        textPanel.setBorder(BorderFactory.createEmptyBorder(100,0,0,0));
         text.setLineWrap(true);
         textPanel.add(text);
         text.setPreferredSize(new Dimension(380,200));
         centerPanel.add(textPanel);
-        
+        JPanel labelPanel = new JPanel();
+        labelPanel.setBorder(BorderFactory.createEmptyBorder(50,0,0,0));
+        labelPanel.add(errorLabel);
+        northPanel.add(labelPanel);
         southPanel.add(runPanel);
         southPanel.add(clearPanel);
+        mainPanel.add(northPanel,BorderLayout.NORTH);
         mainPanel.add(centerPanel,BorderLayout.CENTER);
         mainPanel.add(southPanel,BorderLayout.SOUTH);
         add(mainPanel);
